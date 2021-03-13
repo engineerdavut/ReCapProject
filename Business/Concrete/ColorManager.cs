6aa19185888d0cb23216737d9f1ac6edfb6d0234
Davut Aktaş <engineerdavut@gmail.com>
2021-03-13 19:03:04 +0300

    9.derssonu

---
+++ Business/Concrete/ColorManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ColorManager : IColorService
+    {
+        IColorDal _colorDal;
+
+        public ColorManager(IColorDal colordal)
+        {
+            _colorDal = colordal;
+        }
+
+        public void Add(Color color)
+        {
+            if (color.ColorName.Length > 2)
+            {
+                _colorDal.Add(color);
+                Console.WriteLine("{0} renginiz  veritabanina eklendi"
+                    , color.ColorName);
+            }
+            else
+            {
+                throw new Exception(" 3 harften kucuk renk giremezsiniz");
+            }
+        }
+
+        public List<Color> GetAll()
+        {
+            return _colorDal.GetAll();
+        }
+    }
+}
